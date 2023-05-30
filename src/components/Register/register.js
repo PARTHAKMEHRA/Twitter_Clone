4a4775239f98aa89186ce9f5130189e0cffaa1d9
riya singh <riyafriday5@gmail.com>
2023-05-30 14:49:44 +0530

    connect to local storage

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,5 +1,4 @@
-import React from "react";
-import{ useState } from"react";
+import React, { useState } from 'react';
 import styles from './register.module.css';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
@@ -15,62 +14,79 @@ const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-  border:'none',
-  outline:'none',
-  borderRadius:'15px'
+  border: 'none',
+  outline: 'none',
+  borderRadius: '15px'
 };
 
-const Register= () => {
-
+const Register = () => {
   const [open, setOpen] = React.useState(true);
   const handleClose = () => setOpen(false);
   const navigate = useNavigate();
 
-  function handleSubmit(){
-    navigate('/login')
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    dob: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  function handleSubmit() {
+    const existingData = JSON.parse(localStorage.getItem('registrationData')) || [];
+
+    const newData = [...existingData, formData];
+
+    
+    localStorage.setItem('registrationData', JSON.stringify(newData));
+    navigate('/login');
   }
 
   return (
     <>
-     <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-        <div className={styles.container}>
-
-
-            <div className={styles.heading}>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className={styles.container}>
+              <div className={styles.heading}>
                 <h2>Create your account</h2>
-            </div>
+              </div>
 
-
-            <div className={styles.info}>
-                <input type="name" placeholder='Name' required/>
-                <input type="email" placeholder='Email' required/>
-                <input type="number" placeholder='Phone' required/>
+              <div className={styles.info}>
+                <input type="text" name="name" placeholder="Name" required onChange={handleChange} />
+                <input type="email" name="email" placeholder="Email" required onChange={handleChange} />
+                <input type="number" name="phone" placeholder="Phone" required onChange={handleChange} />
                 <label>Date of birth</label>
-                <p>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
-                <input type="date" placeholder='Month day and year' required/>
+                <p>
+                  This will not be shown publicly. Confirm your own age, even if this account is for a
+                  business, a pet, or something else.
+                </p>
+                <input type="date" name="dob" placeholder="Month day and year" required onChange={handleChange} />
+              </div>
 
+              <div className={styles.button}>
+                <button id={styles.next} onClick={handleSubmit}>
+                  Next
+                </button>
+              </div>
+
+              <div className={styles.not_account}>
+                <p>
+                  Have an account? <a><Link to="/login">Login</Link></a>
+                </p>
+              </div>
             </div>
-
-            <div className={styles.button}>
-                <button id={styles.next} onClick={handleSubmit}>Next</button>
-            </div>
-
-
-            <div className={styles.not_account}>
-                <p>Have an account? <a><Link to="/login">Login</Link></a></p>
-            </div>
-        </div>
-
-        </Box>
-      </Modal>
-    </div>
+          </Box>
+        </Modal>
+      </div>
     </>
   );
 };
